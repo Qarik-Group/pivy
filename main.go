eccacbf28f0b33b88db0f405a18eee2ca86c2664
Ruben Koster <rkoster@starkandwayne.com>
2018-12-04 16:50:11 +0100

    Expose version

--- main.go
+++ main.go
@@ -9,9 +9,12 @@ import (
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
+var Version string
+
 func main() {
 	logger := log.New(os.Stdout, "", 0)
 	app := kingpin.New("pivy", "PivNets little helper")
+	app.Version(Version)
 	commands.Configure(logger, app)
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 }
